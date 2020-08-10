6043f864079cdc975f7fad26c09354b7e0d52d92
cfernhout <fernhoutchiel@gmail.com>
2020-08-10 13:10:42 +0200

    make first model work

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from imageai.Detection.Custom import DetectionModelTrainer
 def main():
     trainer = DetectionModelTrainer()
     trainer.setModelTypeAsYOLOv3()
-    trainer.setDataDirectory(data_directory="/data/raw/headset/")
-    trainer.setTrainConfig(object_names_array=["hololens"], batch_size=4,
-                           num_experiments=100, train_from_pretrained_model="pretrained-yolov3.h5")
+    trainer.setDataDirectory(data_directory="../data/raw/headset")
+    trainer.setTrainConfig(object_names_array=["hololens", "oculus"], batch_size=32,
+                           num_experiments=100, train_from_pretrained_model="../models/pretrained-yolov3.h5")
     trainer.trainModel()
 
 if __name__ == "__main__":
